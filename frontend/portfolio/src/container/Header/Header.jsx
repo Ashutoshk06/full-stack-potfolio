b10cb29750a3caaaa80df9829b7845c4da60cf58
Ashutosh kumar <ashutoshkr042@gmail.com>
2024-04-21 02:54:41 +0530

    responsive header

--- frontend/portfolio/src/container/Header/Header.jsx
+++ frontend/portfolio/src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     const options = {
       strings: ["programmer", "full stack developer"],
       typeSpeed: 50,
-      loop: true
+      loop: false
     };
 
     const typed = new Typed(typedEl.current, options);
@@ -22,19 +22,19 @@ const Header = () => {
   }, []);
 
   return (
-    <div className='main flex w-full h-screen justify-center items-center py-10 overflow-hidden'>
-      <motion.div className='md:w-full w-[50%]'>
-        <span className='block text-[5em] m-10 font-extrabold uppercase text-slate-500'>
+    <div className='main flex w-full h-screen m-10 overflow-hidden'>
+      <motion.div className='md:w-full w-full'>
+        <span className='block text-[2em] sm:text-[2.2em] md:text-[2.6em] lg:text-[3em] m-5 font-extrabold uppercase text-slate-500  w-[60%] sm:w-[70%] h-[30%] md:h-[30%] mb-2'>
           I am a <br />
           <span ref={typedEl} className='multitext text-blue-500'>Programmer</span>
         </span>
-        <p className='w-[90%] block text-[1.2em] m-10 text-slate-500'>
+        <p className='w-[70%] block text-[0.7em] sm:text-[0.8em] mx-5 text-slate-500'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, placeat Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, ab? Repellendus porro, minus tempore voluptatum fuga inventore totam. Autem sequi veniam nisi cupiditate quaerat facilis, odio obcaecati sapiente assumenda deleniti..
         </p>
-        <button className='bg-blue-500 p-2 mx-10 my-2 rounded-md hover:bg-blue-700 hover:text-white shadow-md shadow-blue-300'>
+        <button className='bg-blue-500  p-2 mb-2 mx-5 my-3 rounded-md hover:bg-blue-700 hover:text-white shadow-md shadow-blue-300 '>
           Hire me
         </button>
-        <button className='bg-blue-500 p-2 mx-1 my-2 rounded-md hover:bg-blue-700 hover:text-white shadow-md shadow-blue-300'>
+        <button className='bg-blue-500 p-2 mb-2 mx-1  mr -2 my-3 rounded-md hover:bg-blue-700 hover:text-white shadow-md shadow-blue-300 '>
           Download cv
         </button>
       </motion.div>
